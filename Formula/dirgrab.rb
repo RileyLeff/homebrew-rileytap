89556c3ceca05d638a390274ac1b20f8ae2bc5f9
RileyLeff <rileyleff@gmail.com>
2025-10-13 14:11:30 -0400

    fixing dirgrab

--- Formula/dirgrab.rb
+++ Formula/dirgrab.rb
@@ -2,7 +2,7 @@ class Dirgrab < Formula
   desc "Grab the files that matter from a directory or Git repo and concatenate them"
   homepage "https://github.com/RileyLeff/dirgrab"
   url "https://github.com/RileyLeff/dirgrab/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "4d99c6fea1bee903873f9b86ff02409913a662d3ee59225d6914f569ec6b5f70"
+  sha256 "82079182e7f93c92c6b7997e7ec12f79723df6b35693200ba0ebf0501197737c"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/RileyLeff/dirgrab.git", branch: "main"
 
