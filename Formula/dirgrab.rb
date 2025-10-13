326fcca6d58470df5dce8afd72c4d3f294c02d80
RileyLeff <rileyleff@gmail.com>
2025-10-13 14:07:16 -0400

    adding dirgrab

---
+++ Formula/dirgrab.rb
@@ -0,0 +1,18 @@
+class Dirgrab < Formula
+  desc "Grab the files that matter from a directory or Git repo and concatenate them"
+  homepage "https://github.com/RileyLeff/dirgrab"
+  url "https://github.com/RileyLeff/dirgrab/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "4d99c6fea1bee903873f9b86ff02409913a662d3ee59225d6914f569ec6b5f70"
+  license any_of: ["Apache-2.0", "MIT"]
+  head "https://github.com/RileyLeff/dirgrab.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "dirgrab")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/dirgrab --version")
+  end
+end
