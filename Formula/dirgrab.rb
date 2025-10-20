4b1a7023687941f356f1a6ed3ef67387273cd3e4
RileyLeff <rileyleff@gmail.com>
2025-10-20 13:42:08 -0400

    updating dirgrab new version

--- Formula/dirgrab.rb
+++ Formula/dirgrab.rb
@@ -1,8 +1,8 @@
 class Dirgrab < Formula
   desc "Grab the files that matter from a directory or Git repo and concatenate them"
   homepage "https://github.com/RileyLeff/dirgrab"
-  url "https://github.com/RileyLeff/dirgrab/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "82079182e7f93c92c6b7997e7ec12f79723df6b35693200ba0ebf0501197737c"
+  url "https://github.com/RileyLeff/dirgrab/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "43f471fdf2fb0827115b01ed58c35534ac57732617a54bd48864753e8df907e5"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/RileyLeff/dirgrab.git", branch: "main"
 
